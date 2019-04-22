7725a2f6cbd5e4803b675d8d12e6111363191585
Justin Williams <justinprojects223@hotmail.com>
2019-04-22 15:03:08 -0400

    Servers, Clients, And NEtCat

---
+++ Chapter2/UDPClient.py
@@ -0,0 +1,18 @@
+import socket
+
+target_host = "0.0.0.0"
+target_port = 9999
+
+
+#create a socket object
+client = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+#Send Some Data
+client.sendto("AAABBBCCC", (target_host, target_port))
+
+data, addr = client.recvfrom(4096)
+
+
+print data
+
+
