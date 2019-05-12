5ea2cfc5fd45923d55bccba5e295f21a2a984891
Justin Williams <justinprojects223@hotmail.com>
2019-05-12 15:28:34 -0400

    Netcat finished

--- Chapter2/Netcat.py
+++ Chapter2/Netcat.py
@@ -76,5 +76,207 @@ def main():
 
 			assert False, "Unhandled Option"
 
+
+        if not listen and len(target) and port > 0:
+
+
+                buffer = sys.stdin.read()
+
+
+                client_sender(buffer)
+
+
+
+
+        if listen:
+                server_loop()
+
+
+def client_sender(buff):
+
+        client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+
+        try:
+                client.connect((target, port))
+
+                if len(buff):
+                        client.send(buff)
+
+                        while True:
+
+                                recv_len = 1
+                                response = ""
+
+
+                                while recv_len:
+
+                                        data = client.recv(4096)
+
+                                        recv_len = len(data)
+
+                                        response = response + data
+
+
+                                        if recv_len < 4096:
+
+                                                break
+
+
+                                print response,
+
+                                buff = raw_input("")
+                                buff = buff + "\n"
+
+                                client.send(buff)
+
+
+        except:
+
+                print "[*] Exception! Exiting."
+
+                client.close()
+
+
+
+def server_loop():
+
+        global target
+
+        if not len(target):
+
+                target = "0.0.0.0"
+
+
+        server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        server.bind((target, port))
+
+
+        server.listen(5)
+
+        while True:
+
+                client_socket, addr = server.accept()
+
+                client_thread = threading.Thread(target = client_handler,
+                                                 args = (client_socket,))
+                client_thread.start()
+
+
+
+def run_command(command):
+
+        command = command.rstrip()
+
+
+        try:
+                output = subprocess.check_output(command,
+                                                 stderr = subprocess.STDOUT, shell = True)
+
+        except:
+                output = "Failed to execute command. \r\n"
+
+
+        return output
+
+
+
+
+
+def client_handler(client_socket):
+
+        global upload
+        global execute
+        global command
+
+
+        if len(upload_destination):
+
+                file_buffer = ""
+
+
+
+                while True:
+
+                        data = client_socket.recv(1024)
+
+                        if not data:
+                                break
+                        else:
+                                file_buffer = file_buffer + data
+
+
+                try:
+                        file_descriptor = open(upload_destination, "wb")
+                        file_descriptor.write(file_buffer)
+                        file_descriptor.close()
+
+
+                        text = "Succesfully sacved file to "
+                        text += "%s\r\n" %upload_destination
+                        client_socket.send(text)
+
+
+                except:
+                        text = "Failed to save file "
+                        text += " to %s\r\n" %upload_destination
+                        client.socket.send(text)
+
+
+
+        if len(execute):
+
+                output = run_command(execute)
+
+                client_socket.send(output)
+
+
+        if command:
+
+                while True:
+
+                        client_socket.send("<BHP:#> ")
+
+                        cmd_buffer = ""
+                        while "\n" not in cmd_buffer:
+
+                                cmd_buffer += client_socket.recv(1024)
+
+
+                        response = run_command(cmd_buffer)
+
+                        client_socket.send(response)
+
+                        
+
+
+main()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                
+
+
+
+
 			
 
